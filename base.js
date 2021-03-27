b354b1959c56a30b42f382381874a22bc045a079
Guitarband <60247328+Guitarband@users.noreply.github.com>
2021-03-27 12:44:42 +1100

    Update base.js

--- base.js
+++ base.js
@@ -3,5 +3,5 @@ var total = 0;
 function increment1() {
 	var buttonName = document.getElementById("increment1");
 	total = total + 1;
-	document.getElementById("totalIncrement").value="Total Increments	" + num.toString(total);
+	document.getElementById("totalIncrement").innerHTML="Total Increments	" + num.toString(total);
 }
