9332fc180355adb46c284846d15a0364e9bcfb53
Guitarband <60247328+Guitarband@users.noreply.github.com>
2021-04-01 14:53:06 +1100

    Update base.js

--- base.js
+++ base.js
@@ -12,9 +12,10 @@ link.href = 'main.css';
 
 var increments = 0;
 var autoincrementer1 = 0;
+var output;
 
 function StartUp(){
-	var output = "\nTotal Increments:	0";
+	output = "\nTotal Increments:	0";
 	document.getElementById("totalIncrements").innerHTML = output;
 	//document.getElementById("totalIncrements").innerHTML = output.appendChild(link);
 }
@@ -23,8 +24,8 @@ StartUp();
 
 function Increment(){
 	increments = increments + 1;
-	var output = " Total Increments:	" + increments;
-	document.getElementById("totalIncrements").innerHTML = output;
+	Update();
+	
 	//document.getElementById('totalIncrements').appendChild(link);
 }
 
@@ -32,9 +33,15 @@ function AutoIncrementer(){
 	if(increments >= 10){
 		increments = increments - 10;
 		autoincrementer1 = autoincrementer1 + 1;
-		var output = " Total Increments:	" + increments;
-		document.getElementById("totalIncrements").innerHTML = output;
-		output = " Auto Incrementers:	" + autoincrementer1;
-		document.getElementById("autoIncrementers").innerHTML = output;
+		
+		Update();
 	}
+}
+
+function Update(){
+	output = " Total Increments:	" + increments;
+	document.getElementById("totalIncrements").innerHTML = output;
+	
+	output = " Auto Incrementers:	" + autoincrementer1;
+		document.getElementById("autoIncrementers").innerHTML = output;
 }
