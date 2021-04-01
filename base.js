868ae224e990ebf5d27bbf292c5cadd47c1ca765
Guitarband <60247328+Guitarband@users.noreply.github.com>
2021-04-01 14:27:43 +1100

    Update base.js

--- base.js
+++ base.js
@@ -11,6 +11,7 @@ link.href = 'main.css';
 
 
 var increments = 0;
+var autoincrementer1 = 0;
 
 function StartUp(){
 	var output = "\nTotal Increments:	2";
@@ -25,4 +26,10 @@ function Increment(){
 	var output = " Total Increments:	" + increments;
 	document.getElementById("totalIncrements").innerHTML = output;
 	//document.getElementById('totalIncrements').appendChild(link);
+}
+
+function AutoIncrementer(){
+	autoincrementer1 = autoincrementer1 + 1;
+	var output = " Auto Incrementers:	" + autoincrementer1;
+	document.getElementById("AutoIncrementers").innerHTML = output;
 }
