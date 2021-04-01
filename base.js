1a3d54cedd6994c7b722fd07ea2bd63c3414b5ad
Guitarband <60247328+Guitarband@users.noreply.github.com>
2021-04-01 12:40:34 +1100

    Update base.js

--- base.js
+++ base.js
@@ -1,11 +1,24 @@
+var link = document.createElement('link');
+  
+// set the attributes for link element 
+link.rel = 'stylesheet'; 
+
+link.type = 'text/css';
+
+link.href = 'style.css';
+
+
+
+
 var incrementAmount = 0;
 function StartUp(){
-	document.getElementById("totalIncrements").innerHTML = "\nTotal Increments:	0";
+	var output = "\nTotal Increments:	0";
+	document.getElementById("totalIncrements").innerHTML = output.appendChild(link);
 }
 StartUp();
 
 function Increment(){
 	incrementAmount = incrementAmount + 1;
 	var output = " Total Increments:	" + incrementAmount;
-	document.getElementById("totalIncrements").innerHTML = output;
+	document.getElementById("totalIncrements").innerHTML = output.appendChild(link);
 }
